52c7f10ea7a1308d42566fa4cb23df9036f48e21
Robson Cruz <ghost0092@gmail.com>
2023-12-03 23:57:17 -0300

    day 03: Solve part 1

--- day03/main.go
+++ day03/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"regexp"
+	"strconv"
 	"unicode"
 
 	"github.com/deadpyxel/aod-2023/pkg/aocutils"
@@ -11,29 +13,48 @@ import (
 func convertToSchematic(lines []string) [][]rune {
 	schematic := make([][]rune, len(lines))
 	for i, line := range lines {
-		schematic[i] = make([]rune, len(line))
-		for j, ch := range line {
-			schematic[i][j] = ch
-		}
+		schematic[i] = []rune(line)
 	}
 	return schematic
 }
 
+func isSymbol(r rune) bool {
+	return !(unicode.IsNumber(r) || string(r) == ".")
+}
+
 func solvePart1(schematic [][]rune) (int, error) {
 	var partNumbers []int
+	numRegex := regexp.MustCompile("[0-9]+")
 	for i, line := range schematic {
-		for j, ch := range line {
-			// If the character is not a dot or a number, it is a symbol
-			if !(unicode.IsNumber(ch) || string(ch) == ".") {
-				fmt.Printf("Found symbol [%s] at (%d, %d)\n", string(ch), i, j)
-				if i > 0 && i < len(schematic) {
-
-				}
+		numPos := numRegex.FindAllStringIndex(string(line), -1)
+		for _, pos := range numPos {
+			numStr := string(line[pos[0]:pos[1]])
+			num, _ := strconv.Atoi(numStr)
+			if isAdjacentToSymbol(schematic, i, pos[0], len(numStr)) {
+				partNumbers = append(partNumbers, num)
 			}
 		}
 	}
 
 	return aocutils.Sum(partNumbers), nil
+}
+
+func isAdjacentToSymbol(schematic [][]rune, i, j, length int) bool {
+	for di := -1; di <= 1; di++ {
+		for dj := -1; dj <= length; dj++ {
+			if isOutOfBounds(schematic, i+di, j+dj) {
+				continue
+			}
+			if isSymbol(schematic[i+di][j+dj]) {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func isOutOfBounds(schematic [][]rune, i, j int) bool {
+	return i < 0 || i >= len(schematic) || j < 0 || j >= len(schematic[i])
 }
 
 func main() {
@@ -43,7 +64,6 @@ func main() {
 		log.Fatalf("Error reading input data: %v\n", err)
 	}
 	schematic := convertToSchematic(lines)
-	fmt.Printf(" Schematic:\n%v\n", schematic)
 	sol, err := solvePart1(schematic)
 	if err != nil {
 		log.Fatalf("Found error solving Problem 3-1: %v\n", err)
